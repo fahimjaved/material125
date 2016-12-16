ab4118f2b1b63cab1ed6602fcf43d2dcf18acd9c
Mudassar Rehman <mudassar.rehman786@gmail.com>
2016-12-16 13:07:14 +0500

    test

--- config/env/development.js
+++ config/env/development.js
@@ -37,27 +37,27 @@ module.exports = {
   facebook: {
     clientID: 'APP_ID',
     clientSecret: 'APP_SECRET',
-    callbackURL: 'http://localhost:3000/auth/facebook/callback'
+    callbackURL: 'http://ec2-35-160-199-169.us-west-2.compute.amazonaws.com:3001/auth/facebook/callback'
   },
   twitter: {
     clientID: 'CONSUMER_KEY',
     clientSecret: 'CONSUMER_SECRET',
-    callbackURL: 'http://localhost:3000/auth/twitter/callback'
+    callbackURL: 'http://ec2-35-160-199-169.us-west-2.compute.amazonaws.com:3001/auth/twitter/callback'
   },
   github: {
     clientID: 'APP_ID',
     clientSecret: 'APP_SECRET',
-    callbackURL: 'http://localhost:3000/auth/github/callback'
+    callbackURL: 'http://ec2-35-160-199-169.us-west-2.compute.amazonaws.com:3001/auth/github/callback'
   },
   google: {
     clientID: 'APP_ID',
     clientSecret: 'APP_SECRET',
-    callbackURL: 'http://localhost:3000/auth/google/callback'
+    callbackURL: 'http://ec2-35-160-199-169.us-west-2.compute.amazonaws.com:3001/auth/google/callback'
   },
   linkedin: {
     clientID: 'API_KEY',
     clientSecret: 'SECRET_KEY',
-    callbackURL: 'http://localhost:3000/auth/linkedin/callback'
+    callbackURL: 'http://ec2-35-160-199-169.us-west-2.compute.amazonaws.com:3001/auth/linkedin/callback'
   },
   emailFrom: 'SENDER EMAIL ADDRESS', // sender address like ABC <abc@example.com>
   mailer: {
